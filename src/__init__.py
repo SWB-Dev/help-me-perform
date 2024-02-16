6da8088dd03a5d921308c0927fe1da0725f11945
Barnes, Steven <Steven.Barnes@usfoods.com>
2024-02-16 18:24:25 -0500

    adds base path for sql inserts

--- src/__init__.py
+++ src/__init__.py
@@ -3,13 +3,23 @@ DATA_PATH = os.path.join("data")
 DB_PATH = os.path.join(DATA_PATH, "hmpdb.db")
 SRC_PATH  = os.path.join("src")
 SQL_PATH = os.path.join(SRC_PATH, "sql")
+SQL_INSERTS_PATH = os.path.join(SQL_PATH, "inserts")
 SQL_FILES = {
     "BUILD": os.path.join(SQL_PATH, "build.sql"),
     "INSERT": {
-        "GROUP": os.path.join(SQL_PATH, "inserts", "insert_group.sql"),
-        "STAKEHOLDER": os.path.join(SQL_PATH, "inserts", "insert_stakeholder.sql")
+        "GROUP": os.path.join(SQL_INSERTS_PATH, "insert_group.sql"),
+        "STAKEHOLDER": os.path.join(SQL_INSERTS_PATH, "insert_stakeholder.sql"),
+        "KPI": os.path.join(SQL_INSERTS_PATH, "insert_kpi.sql")
     }
 }
+
+
+def parse_required_tables():
+    contents = ""
+    with open(SQL_FILES["BUILD"], "r") as f:
+        contents = f.readlines()
+    table_names = [x.partition(" ")[-2] for x in contents if "TABLE" in x]
+    return table_names
 
 
 # from enums import Status, Granularity
