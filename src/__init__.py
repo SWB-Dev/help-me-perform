014c5cf966b94b1bce4b98514d412e05de9a758b
SWB <steven.barnes@swbdevelopment.com>
2024-02-17 00:49:32 -0500

    adds import structure, refactor require tables fx

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,9 @@
 import os
+import sys
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(SCRIPT_DIR))
+print(SCRIPT_DIR)
+
 DATA_PATH = os.path.join("data")
 DB_PATH = os.path.join(DATA_PATH, "hmpdb.db")
 SRC_PATH  = os.path.join("src")
@@ -14,12 +19,15 @@ SQL_FILES = {
 }
 
 
-def parse_required_tables():
+def parse_required_tables() -> tuple[list[str], list[str]]:
     contents = ""
+    sql = []
     with open(SQL_FILES["BUILD"], "r") as f:
         contents = f.readlines()
-    table_names = [x.partition(" ")[-2] for x in contents if "TABLE" in x]
-    return table_names
+        f.seek(0)
+        sql = f.read().split(";")
+    table_names = [x.split(" ")[-2] for x in contents if "TABLE" in x]
+    return (table_names, sql,)
 
 
 # from enums import Status, Granularity
@@ -31,4 +39,4 @@ def parse_required_tables():
 # from interfaces.ITask import ITask
 
 
-from models import db
+from .models import db
