7e658394b258fba98c43941643c57b7909a4a67f
SWB <steven.barnes@swbdevelopment.com>
2024-02-17 00:51:47 -0500

    refactor to use query builder, adds select command

--- src/cli.py
+++ src/cli.py
@@ -8,10 +8,12 @@ sys.path.append(os.path.dirname(SCRIPT_DIR))
 
 import src
 
-db = src.db.DefaultDB()
+# db = src.db.DefaultDB()
+db = src.db.TestDB()
 
 PROMPT = "HMP> "
-COMMANDS = ["EXIT", "INSERT"]
+COMMANDS = ["EXIT", "INSERT", "SELECT"]
+CACHE = {}
 
 def inform(s:str, file=None) -> None:
     print(f"{PROMPT}{s}",file=file)
@@ -27,9 +29,10 @@ def setup():
     inform("Setting up cli...")
     os.makedirs(src.DATA_PATH, exist_ok=True)
     inform("Initializing database...")
-    db.initialize()
+    db.initialize_db()
 
 def handle_insert(cmds:list[str]):
+    _cmd_name = "insert"
     t = ""
     v = ()
     options = ["-t", "-d"]
@@ -45,24 +48,67 @@ def handle_insert(cmds:list[str]):
             v = v + (cmd,)
 
     if t == "":
-        inform_error("Command \"insert\" is missing option \"-t\"")
+        inform_error(f"Command \"{_cmd_name}\" is missing option \"-t\"")
         has_error = True
     if len(v) == 0:
-        inform_error("Command \"insert\" is missing option \"-d\"")
+        inform_error(f"Command \"{_cmd_name}\" is missing option \"-d\"")
         has_error = True
     
     if has_error:
         return
 
-    result = db.insert_one(t, v)
+    result = db.table(t).insert(v).execute()
     return result
 
+def handle_select(cmds:list[str]):
+    _cmd_name = "select"
+    t = ""
+    f = ""
+    k = ""
+    v = ()
+    options = ["-t", "-f", "-v", "-store"]
+    seeking = ""
+    has_error = False
+
+    for cmd in cmds:
+        if cmd.lower() in options:
+            seeking = cmd.lower()
+        elif seeking == "-t":
+            t = cmd.upper()
+        elif seeking == "-f":
+            f = cmd.upper()
+        elif seeking == "-v":
+            v = v + (cmd,)
+        elif seeking == "-store":
+            k = cmd
+
+    if t == "":
+        inform_error(f"Command \"{_cmd_name}\" is missing option \"-t\"")
+        has_error = True
+    if len(v) == 0:
+        inform_error(f"Command \"{_cmd_name}\" is missing option \"-v\"")
+        has_error = True
+    if f == "":
+        inform_error(f"Command \"{_cmd_name}\" is missing option \"-f\"")
+        has_error = True
+    
+    if has_error:
+        return
+    result = None
+    if f == "ID":
+        result = db.table(t).find_one_by_id(v[0]).execute()
+    if k != "":
+        CACHE[k] = result
+
+    return result
 
 def main():
     """"""
     setup()
     while True:
-        raw = shlex.split(get_input())
+        raw_cmd = get_input()
+        param_cnt = raw_cmd.count("-")
+        raw = shlex.split(raw_cmd)
         cmd, params = raw[0], raw[1:]
         ucmd = cmd.upper()
 
@@ -71,7 +117,7 @@ def main():
                 break
 
             if ucmd == "INSERT":
-                if len(params) < 2:
+                if param_cnt < 2:
                     inform_error(f"Too few parameters for command \"{cmd}\"")
                 else:
                     retval = handle_insert(params)
@@ -79,9 +125,19 @@ def main():
                         inform_error(retval[1])
                     elif retval:
                         inform(retval)
+            elif ucmd == "SELECT":
+                if param_cnt < 3:
+                    inform_error(f"Too few parameters for command \"{cmd}\"")
+                else:
+                    retval = handle_select(params)
+                    if retval and retval[0] == "ERROR":
+                        inform_error(retval[1])
+                    elif retval:
+                        inform(retval)
         else:
             inform_error(f"Invalid command \"{cmd}\"")
-
+    
+    db.close()
     print("Exiting HMP...")
 
 if __name__ == "__main__":
